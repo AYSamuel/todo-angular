7b63c216692f1c38f7ed62f4157137aa531494b1
Ayomide <ayomide.morayo.bankole@buildingminds.com>
2023-08-18 11:48:22 +0200

    more feat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,30 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CreateComponent } from './create/create.component';
 
+import { provideSvgIcons } from '@ngneat/svg-icon';
+import { SvgIconComponent } from '@ngneat/svg-icon';
+import { provideSvgIconsConfig } from '@ngneat/svg-icon';
+
+import { deleteIcon } from '@app/svg/delete';
+import { doneIcon } from '@app/svg/done';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    CreateComponent
+  declarations: [AppComponent, NavbarComponent, CreateComponent],
+  imports: [BrowserModule, AppRoutingModule, SvgIconComponent],
+  providers: [
+    provideSvgIcons([deleteIcon, doneIcon]),
+    provideSvgIconsConfig({
+      sizes: {
+        xs: '10px',
+        sm: '12px',
+        md: '16px',
+        lg: '20px',
+        xl: '25px',
+        xxl: '30px',
+      },
+      defaultSize: 'md', // Default size to be used
+    }),
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
