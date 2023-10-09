beab980e93e35cbffb728ebf24a751f9ca6723c5
Ayomide <ayomide.morayo.bankole@buildingminds.com>
2023-10-09 09:37:27 +0200

    home added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { HomeComponent } from './routes/home/home.component';
+import { CompletedComponent } from './routes/completed/completed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CreateComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'create',
+    component: CreateComponent,
+  },
+  {
+    path: 'completed',
+    component: CompletedComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/routes/completed/completed.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '@app/navbar/navbar.component';
+
+@Component({
+  selector: 'app-completed',
+  templateUrl: './completed.component.html',
+  styleUrls: ['./completed.component.css'],
+})
+export class CompletedComponent {}
